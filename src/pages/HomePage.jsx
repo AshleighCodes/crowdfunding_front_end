871db4def7ba19a76de39c28c9fd9d31d2f0b747
Ashleigh Wilson <ashleigh.morgan.wilson@gmail.com>
2024-12-09 16:56:34 +1000

    modified HomePage.jsx and css

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,3 +1,23 @@
+// import useProjects from "../hooks/use-projects";
+// import ProjectCard from "../components/ProjectCard";
+// import ProjectForm from "../components/ProjectForm";
+// import "../pages/HomePage.css";
+
+// function HomePage() {
+//     const { projects } = useProjects();
+//     return (
+//         <div id="project-list">
+//             {projects.map((projectData, key) => {
+//                 return <ProjectCard key={key} projectData={projectData} />; 
+//             })}
+//             <ProjectForm />
+//         </div>
+//     );
+// }
+
+// export default HomePage;
+
+import React from 'react';
 import useProjects from "../hooks/use-projects";
 import ProjectCard from "../components/ProjectCard";
 import ProjectForm from "../components/ProjectForm";
@@ -6,10 +26,13 @@ import "../pages/HomePage.css";
 function HomePage() {
     const { projects } = useProjects();
     return (
-        <div id="project-list">
-            {projects.map((projectData, key) => {
-                return <ProjectCard key={key} projectData={projectData} />; 
-            })}
+        <div className="home-container">
+            <h1 className="home-title">Our Projects</h1>
+            <div className="project-list">
+                {projects.map((projectData, key) => (
+                    <ProjectCard key={key} projectData={projectData} />
+                ))}
+            </div>
             <ProjectForm />
         </div>
     );
